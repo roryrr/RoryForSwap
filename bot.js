bb94c11075e72ba740d4e4e5cc8b618dacb322e7
pavanulichi <pavan.ulichi@gmail.com>
2017-12-01 11:53:09 -0500

    error handled

--- bot.js
+++ bot.js
@@ -220,8 +220,11 @@ app.post('/ai', (req, res) => {
         console.log(body);
         console.log("powerranger");
         console.log(GLOBAL_PRODUCT_NAME);
-        if (body.placements[0].numFound == "0") {
-          sendTextMessage(GLOBAL_ID, "Oops, looks like we don’t have anything that fits that description.")
+        if(body.message == "Error occurred while performing the search") {
+          sendTextMessage(GLOBAL_ID, "Please try again");
+        }
+        else if (body.placements[0].numFound == "0") {
+          sendTextMessage(GLOBAL_ID, "Oops, looks like we don’t have anything that fits that description.");
         }
         else{
               rr_array = body.placements[0].docs;
