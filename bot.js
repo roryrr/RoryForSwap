c27f3b2b65c9580a3c4eee5dfb13c006936e1af5
pavanulichi <pavan.ulichi@gmail.com>
2017-12-01 12:05:50 -0500

    error handled

--- bot.js
+++ bot.js
@@ -479,28 +479,33 @@ function receivedMessage(event) {
                 rows= "9";
           var  requesting = req_url + "?apiClientKey=" + apiClientKey + "&userId=" + userId + "&sessionId=" + sessionId + "&placements=" + placements + "&lang=en&start=0&rows=9&query=" + query + "&filter=" + GLOBAL_PRODUCT_BRAND + "&filter=" + GLOBAL_PRODUCT_GENDER + "&filter=" + GLOBAL_PRODUCT_COLOR + "&filter=" + GLOBAL_PRODUCT_SIZE;
           console.log(requesting);
-            request(requesting, function (error, response, body) {
-                  if (!error && response.statusCode == 200) {
-                    //parsing the json response from RR cloud
-                    body = JSON.parse(body);
-                    console.log("powerranger");
-                    console.log(GLOBAL_PRODUCT_NAME);
-                    if(body.message == "Error occurred while performing the search") {
-                      sendTextMessage(GLOBAL_ID, "Please try again");
-                    }
-                    else if (body.placements[0].numFound == "0") {
-                      sendTextMessage(GLOBAL_ID, "Oops, looks like we don’t have anything that fits that description.")
-                    }
-                    else{
-                          rr_array = body.placements[0].docs;
-                          sendGenericMessageForSearch(GLOBAL_ID, rr_array);
-                          setTimeout(function() { v2_sendFilters(GLOBAL_ID, GLOBAL_PRODUCT_NAME) }, 3000);
-                          // setTimeout(function() { v2_restartAnytime(GLOBAL_ID) }, 7000);
-                        }
-                // The Description is:  "descriptive string"
-              } else {
-              console.log('Pavan api.ai, ERROR 6');
+          var options = {
+            uri: requesting,
+            headers: {
+              'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; A1 Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Mobile Safari/537.36'
+              },
+            json: true
+            };
+          reqPromise(options)
+            .then(function(body){
+              console.log("powerranger");
+              console.log(GLOBAL_PRODUCT_NAME);
+              if(body.message == "Error occurred while performing the search") {
+                sendTextMessage(GLOBAL_ID, "Please try again");
               }
+              else if (body.placements[0].numFound == "0") {
+                sendTextMessage(GLOBAL_ID, "Oops, looks like we don’t have anything that fits that description.")
+              }
+              else{
+                    rr_array = body.placements[0].docs;
+                    sendGenericMessageForSearch(GLOBAL_ID, rr_array);
+                    setTimeout(function() { v2_sendFilters(GLOBAL_ID, GLOBAL_PRODUCT_NAME) }, 3000);
+                    // setTimeout(function() { v2_restartAnytime(GLOBAL_ID) }, 7000);
+                  }
+          // The Description is:  "descriptive string"
+            })
+            .catch(function(err){
+              console.log('Pavan api.ai, ERROR 6');
             });
         }
         else if (derivedPayload == "v2_restart") {
